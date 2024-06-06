0940eeda7268601a9338207681d15ca03c45e0e9
Dwarfilicious <zoeteman1@gmail.com>
2024-06-06 12:07:46 +0200

    added resync button and fix ProfileDropdown indentation

--- frontend/src/components/profile/ProfileDropdown.tsx
+++ frontend/src/components/profile/ProfileDropdown.tsx
@@ -4,6 +4,7 @@ import {
     Settings,
     User,
     GraduationCap,
+    RefreshCcw,
 } from "lucide-react"
 import { Button } from "../ui/Button.tsx"
 import {
@@ -62,57 +63,68 @@ export const ProfileDropdown: FC<Profile> = (initProfile): ReactElement => {
     }
 
     return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="profile">{profile.account}</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>{profile.account}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <span>Profile</span>
-            </DropdownMenuItem>
-            <DropdownMenuSub>
-                <DropdownMenuSubTrigger>
-                <GraduationCap className="mr-2 h-4 w-4" />
-                <span>View as: {roleToString(profile.simRole)}</span>
-                </DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                    {profile.simRole !== Role.Student && hasRights(profile.role, Role.Student) && (
-                        <DropdownMenuItem onClick={() => setSimRole(Role.Student)}>
-                        <GraduationCap className="mr-2 h-4 w-4" />
-                        <span>Student</span>
-                        </DropdownMenuItem>
-                    )}
-                    {profile.simRole !== Role.TA && hasRights(profile.role, Role.TA) && (
-                        <DropdownMenuItem onClick={() => setSimRole(Role.TA)}>
-                        <GraduationCap className="mr-2 h-4 w-4" />
-                        <span>TA</span>
-                        </DropdownMenuItem>
-                    )}
-                    {profile.simRole !== Role.Teacher && hasRights(profile.role, Role.Teacher) && (
-                        <DropdownMenuItem onClick={() => setSimRole(Role.Teacher)}>
-                        <GraduationCap className="mr-2 h-4 w-4" />
-                        <span>Teacher</span>
-                        </DropdownMenuItem>
-                    )}
-                </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuItem>
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={handleLogout}>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="profile">{profile.account}</Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>{profile.account}</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <User className="mr-2 h-4 w-4" />
+                        <span>Profile</span>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                            <GraduationCap className="mr-2 h-4 w-4" />
+                            <span>View as: {roleToString(profile.simRole)}</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                {profile.simRole !== Role.Student && hasRights(profile.role, Role.Student) && (
+                                    <DropdownMenuItem onClick={() => setSimRole(Role.Student)}>
+                                    <GraduationCap className="mr-2 h-4 w-4" />
+                                    <span>Student</span>
+                                    </DropdownMenuItem>
+                                )}
+                                {profile.simRole !== Role.TA && hasRights(profile.role, Role.TA) && (
+                                    <DropdownMenuItem onClick={() => setSimRole(Role.TA)}>
+                                    <GraduationCap className="mr-2 h-4 w-4" />
+                                    <span>TA</span>
+                                    </DropdownMenuItem>
+                                )}
+                                {profile.simRole !== Role.Teacher && hasRights(profile.role, Role.Teacher) && (
+                                    <DropdownMenuItem onClick={() => setSimRole(Role.Teacher)}>
+                                    <GraduationCap className="mr-2 h-4 w-4" />
+                                    <span>Teacher</span>
+                                    </DropdownMenuItem>
+                                )}
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+
+                    <DropdownMenuItem>
+                        <RefreshCcw className="mr-2 h-4 w-4" />
+                        <span>Resync</span>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <Settings className="mr-2 h-4 w-4" />
+                        <span>Settings</span>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem onClick={handleLogout}>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 }
