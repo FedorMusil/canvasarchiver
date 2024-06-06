fca698c3e8a46a6096251587c4daec4644285476
TijnvdK <tijn.van.den.kommer@student.uva.nl>
2024-06-06 13:03:30 +0200

    feat (frontend): Setup browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Home from './components/pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FC, ReactElement } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -8,7 +9,12 @@ const App: FC = (): ReactElement => {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+
             {import.meta.env.MODE === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
         </QueryClientProvider>
     );
