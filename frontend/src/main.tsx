4c89892959f0c36ddcf28bfbe86086e0768c9394
DanioHans <dainkooijman@gmail.com>
2024-06-07 21:01:34 +0200

    re-enabled mocking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './globals.css';
 
-// async function enableMocking() {
-//     if (import.meta.env.MODE !== 'mock') return;
+async function enableMocking() {
+    if (import.meta.env.MODE !== 'mock') return;
 
-//     const { worker } = await import('./mock/setupWorker.ts');
-//     return await worker.start({
-//         onUnhandledRequest: 'bypass',
-//     });
-// }
+    const { worker } = await import('./mock/setupWorker.ts');
+    return await worker.start({
+        onUnhandledRequest: 'bypass',
+    });
+}
 
-// enableMocking().then(() => {
-//     ReactDOM.createRoot(document.getElementById('root')!).render(
-//         <React.StrictMode>
-//             <App />
-//         </React.StrictMode>
-//     );
-// });
+enableMocking().then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
