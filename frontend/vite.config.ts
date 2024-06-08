7c6bcff4549adab75afd73cb1717c9c6db110e3c
TijnvdK <tijn.van.den.kommer@student.uva.nl>
2024-06-08 08:32:23 +0200

    feat (frontend): Add test environment with `vitest`.
    To make the test environment work with the mock environment I had to split the mock environment in a server and browser part. The browser part automatically starts with `npm run mock`, while the server part starts automatically with the test environment.
    
    To start the test environment run `npm run test:console` or `npm run test:ui`.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/__tests__/setupTests.ts',
     },
 });
