1774bb4e95862888d8e08e3553c697f4766ce27e
Dwarfilicious <zoeteman1@gmail.com>
2024-06-11 13:27:01 +0200

    fixed the vite.config.ts file for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,25 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    https: {
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+    },
+    server: {
+        /* https: {
       key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
+    }, */
+        port: 3000,
+        /* cors: {
+      // TODO: Change the cors origin to something safer
+      origin: '*', // Allow all origins, or specify the exact origins you want to allow
+    } */
     },
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/__tests__/setupTests.ts',
     },
-    port: 3000,
-    cors: {
-      // TODO: Change the cors origin to something safer
-      origin: '*', // Allow all origins, or specify the exact origins you want to allow
-    }
-  }
 });
