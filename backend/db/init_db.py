3ae24d45e9693691296ac86c92f0cdac0b3ee0a6
Dwarfilicious <zoeteman1@gmail.com>
2024-06-12 13:14:02 +0200

    changed database implementation (should be final)

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -1,7 +1,10 @@
 from get_db_conn import get_db_conn
 
+
 # Warning Do not run this script unless you want to destroy the existing tables and recreate them.
-# This script is used to create the tables in the database. It will first drop the existing tables if destroy_existing_tables is True.
+# This script is used to create the tables in the database. It will first
+# drop the existing tables if destroy_existing_tables is True.
+
 
 def create_tables(destroy_existing_tables=False):
     '''Creates the tables in the database. If destroy_existing_tables is True, it will first drop the existing tables.
@@ -11,24 +14,23 @@ def create_tables(destroy_existing_tables=False):
 
     if destroy_existing_tables:
 
-        print ('Destroying existing tables...')
+        print('Destroying existing tables...')
         cur.execute('''
-                    
+
 
         DROP TABLE IF EXISTS annotations;
         DROP TABLE IF EXISTS changes;
         DROP TABLE IF EXISTS users;
         DROP TABLE IF EXISTS courses;
-                    
+
         DROP TYPE IF EXISTS change_type;
         DROP TYPE IF EXISTS item_types;
         DROP TYPE IF EXISTS user_role;
-        
+
         ''')
         conn.commit()
 
-
-    print ('Creating tables...')
+    print('Creating tables...')
     cur.execute('''
     CREATE TYPE change_type AS ENUM ('Deletion', 'Addition', 'Modification');
     CREATE TYPE item_types AS ENUM ('Assignments', 'Pages', 'Files', 'Quizzes', 'Modules', 'Sections');
@@ -39,28 +41,29 @@ def create_tables(destroy_existing_tables=False):
         name TEXT NOT NULL,
         course_code TEXT NOT NULL UNIQUE
     );
-                
+
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         email TEXT NOT NULL UNIQUE,
         name TEXT NOT NULL
     );
-                
+
     CREATE TABLE IF NOT EXISTS teacher_courses (
         user_id INT REFERENCES users(id),
         course_id INT REFERENCES courses(id),
         role user_role NOT NULL,
         PRIMARY KEY (user_id, course_id)
-    );    
+    );
 
     CREATE TABLE IF NOT EXISTS changes (
         id SERIAL PRIMARY KEY,
+        item_id INT NOT NULL,
         course_id INT REFERENCES courses(id),
         change_type change_type NOT NULL,
         timestamp TIMESTAMP NOT NULL,
         item_type item_types NOT NULL,
-        old_value JSON,
-        new_value JSON
+        older_diff INT REFERENCES changes(id),
+        diff JSON
     );
 
     CREATE TABLE IF NOT EXISTS annotations (
@@ -76,6 +79,7 @@ def create_tables(destroy_existing_tables=False):
     cur.close()
     conn.close()
 
+
 if __name__ == '__main__':
-    create_tables(destroy_existing_tables=True)
-    print ('Tables created successfully.')
+    create_tables(destroy_existing_tables=False)
+    print('Tables created successfully.')
