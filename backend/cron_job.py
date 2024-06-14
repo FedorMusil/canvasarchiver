37bf2e9956e9e56b989f6511c2f47f5f757f6697
Dwarfilicious <zoeteman1@gmail.com>
2024-06-14 17:47:28 +0200

    wrote first database insertions and insertion unit test

--- backend/cron_job.py
+++ backend/cron_job.py
@@ -1,22 +1,68 @@
 import canvas.canvas as canvasapi
 import canvas.connection as connection
+from db.get_db_conn import get_db_conn
+import psycopg2.extras
+import json
 import asyncio
 
 
-async def course_diff(course):
+async def handle_course(course):
     cdata = course.get_data()
-    print(f"Course: {cdata['id']}, {cdata['name']}")
+
+    with get_db_conn() as db_conn:
+        cur = db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        cur.execute('''
+            SELECT * FROM courses
+            WHERE course_code = %s
+        ''', (cdata['course_code'],))
+
+        result = cur.fetchone()
+        if result:
+            return result['id'], True
+        else:
+            cur.execute('''
+                INSERT INTO courses (name, course_code)
+                VALUES (%s, %s)
+                RETURNING id
+            ''', (cdata['name'], cdata['course_code']))
+            db_conn.commit()
+            return cur.fetchone()['id'], False
 
 
-async def page_diffs(api, course):
+async def save_new_course(course, course_id):
+    cdata = course.get_data()
+
+    with get_db_conn() as db_conn:
+        cur = db_conn.cursor()
+
+        cur.execute('''
+            INSERT INTO changes (course_id, change_type, timestamp, item_type, diff)
+            VALUES (%s, 'Addition', NOW(), 'Course', %s)
+        ''', (course_id, json.dumps(cdata)))
+
+
+async def save_course_pages(api, course, course_id):
+    async for page in api.get_pages(course):
+        pdata = page.get_data()
+        with get_db_conn() as db_conn:
+            cur = db_conn.cursor()
+
+            cur.execute('''
+                INSERT INTO changes (course_id, change_type, timestamp, item_type, diff)
+                VALUES (%s, 'Addition', NOW(), 'Pages', %s)
+            ''', (course_id, json.dumps(pdata)))
+
+
+async def page_diffs(api, course, course_id):
     async for page in api.get_pages(course):
         pdata = page.get_data()
         print(f"Page: {pdata['page_id']}, {pdata['title']}")
 
 
-async def dir_diffs(api, dir):
+async def dir_diffs(api, dir, course_id):
     for name, subdir in dir.items():
-        await dir_diffs(api, subdir)
+        await dir_diffs(api, subdir, course_id)
     folder = dir.get_folder()
     if folder is not None:
         async for file in api.get_files(folder=folder):
@@ -24,9 +70,9 @@ async def dir_diffs(api, dir):
             print(f"File: {fdata['id']}, {fdata['display_name']}")
 
 
-async def filesystem_diffs(api, course):
+async def filesystem_diffs(api, course, course_id):
     dir = await api.get_folders(course=course)
-    await dir_diffs(api, dir)
+    await dir_diffs(api, dir, course_id)
 
 
 async def main():
@@ -34,9 +80,16 @@ async def main():
         api = canvasapi.Canvas(conn)
 
         async for course in api.get_courses():
-            await course_diff(course)
-            await page_diffs(api, course)
-            await filesystem_diffs(api, course)
+            course_id, is_new = await handle_course(course)
+            print(f"Course: {course_id}")
+            if is_new:
+                # store new course data
+                await save_new_course(course, course_id)
+                await save_course_pages(api, course, course_id)
+            else:
+                # calculate and store diffs
+                await page_diffs(api, course, course_id)
+                await filesystem_diffs(api, course, course_id)
 
 
 if __name__ == "__main__":
---
+++ backend/test_db_insertion.py
@@ -0,0 +1,66 @@
+import unittest
+import json
+from db.get_db_conn import get_db_conn
+import psycopg2.extras
+
+
+class TestDatabaseInsertion(unittest.TestCase):
+    def setUp(self):
+        self.db_conn = get_db_conn()
+        self.cur = self.db_conn.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+
+    def tearDown(self):
+        self.cur.close()
+        self.db_conn.close()
+
+    def test_insertion(self):
+        cdata = {
+            'name': 'Random Course',
+            'course_code': 'HIHI69RAND'
+        }
+
+        self.cur.execute('''
+            INSERT INTO courses (name, course_code)
+            VALUES (%s, %s)
+            RETURNING id
+        ''', (cdata['name'], cdata['course_code']))
+        self.db_conn.commit()
+        course_id = self.cur.fetchone()['id']
+
+        self.cur.execute('''
+            INSERT INTO changes (course_id, change_type, timestamp, item_type, diff)
+            VALUES (%s, 'Addition', NOW(), 'Course', %s)
+            RETURNING id
+        ''', (course_id, json.dumps(cdata)))
+        self.db_conn.commit()
+
+        change_id = self.cur.fetchone()['id']
+
+        self.cur.execute('''
+            SELECT * FROM changes
+            WHERE id = %s
+        ''', (change_id,))
+
+        result = self.cur.fetchone()
+
+        self.assertEqual(result['course_id'], course_id)
+        self.assertEqual(result['change_type'], 'Addition')
+        self.assertEqual(result['item_type'], 'Course')
+        self.assertEqual(result['diff'], cdata)
+
+        self.cur.execute('''
+            DELETE FROM changes
+            WHERE id = %s
+        ''', (change_id,))
+        self.db_conn.commit()
+
+        self.cur.execute('''
+            DELETE FROM courses
+            WHERE id = %s
+        ''', (course_id,))
+        self.db_conn.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
