5f044479325c6ccf2fdf1ec9b435aec097a1057d
Fedor Musil <fedor.musil@student.uva.nl>
2024-06-15 21:10:56 +0000

    Update of program.py and acording libaries, they now all use FastAPI, also the backend api is upgraded to be all async for all i/o operations done which should be way faster. Also added support for JWT token security which is only enabled on the /self route for now but can easily be added to other routes later. The code still needs to be cleaned up a bit and tested better before being pushed to production.

--- backend/db/get_db_conn.py
+++ backend/db/get_db_conn.py
@@ -1,46 +1,40 @@
-import psycopg2
-from psycopg2 import sql
+import asyncpg
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv(dotenv_path='../../.env', encoding='utf-8')
+db_params = {
+    'user': getenv('DB_USER'),
+    'password': getenv('DB_PASSWORD'),
+    'host': getenv('DB_HOST'),
+    'port': getenv('DB_PORT'),
+    'database': 'postgres'  # connect to default database to execute CREATE DATABASE command
+}
 
-def get_db_conn():
-    # Define your database connection parameters
-    db_params = {
-        'user': getenv('DB_USER'),
-        'password': getenv('DB_PASSWORD'),
-        'host': getenv('DB_HOST'),
-        'port': getenv('DB_PORT'),
-        'dbname': 'postgres'  # connect to default database to execute CREATE DATABASE command
-    }
 
+async def get_db_conn():
     # Connect to your postgres DB
-    conn = psycopg2.connect(**db_params)
-
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
+    conn = await asyncpg.connect(**db_params)
 
     # Check if database exists
-    cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (getenv('DB_NAME'),))
-    exists = cur.fetchone()
+    exists = await conn.fetchrow("SELECT 1 FROM pg_catalog.pg_database WHERE datname = $1", getenv('DB_NAME'))
     if not exists:
         # Can't create database from within another connection, so disconnect
-        cur.close()
-        conn.close()
+        await conn.close()
 
         # Connect to the default database to create the new database
-        conn = psycopg2.connect(**db_params)
-        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-        cur = conn.cursor()
-        cur.execute(sql.SQL("CREATE DATABASE {}").format(
-            sql.Identifier(getenv('DB_NAME')))
-        )
+        conn = await asyncpg.connect(**db_params)
+        await conn.execute(f"CREATE DATABASE {getenv('DB_NAME')}")
+ 
+    return conn
 
-    # Close the cursor and connection
-    cur.close()
-    conn.close()
 
-    # Connect to the new database and return the connection
-    db_params['dbname'] = getenv('DB_NAME')
-    return psycopg2.connect(**db_params)
+async def create_pool():
+    pool = await asyncpg.create_pool(**db_params)
+    return pool
+
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(get_db_conn())
+    print('Connected to database successfully.')
