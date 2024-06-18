545d8986880426e21268126408d9882db10d0b56
PC <melmajouti@gmail.com>
2024-06-18 16:13:38 +0200

    fixed styling to match canvas

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -20,12 +20,12 @@ const Home: FC = (): ReactElement => {
             {isError && <div>Error</div>}
             {/* {data && <div data-testid='home-account-data'>{JSON.stringify(data)}</div>} */}
             {data && (
-                <div className="p-16 w-1/2">
+                <div className="p-16">
                     <ul className="flex flex-wrap gap-4 justify-start">
                         {data.map((section: Section) => (
-                            <li key={section.id} className="bg-gray-100 w-40 h-40 rounded-md p-3 hover:bg-red-200 border border-gray-300 flex-shrink-0">
-                                <h3>Name: {section.name}</h3>
-                                <p>Id: {section.id}</p>
+                            <li key={section.id} className="bg-neutral-100 w-40 h-40 rounded p-3 hover:bg-blue-50 border border-gray-300 flex-shrink-0">
+                                <a className="hover:underline font-medium" href="#">{section.name}</a>
+                                <p>id: {section.id}</p>
                                 {/* Render other properties as needed */}
                             </li>
                         ))}
