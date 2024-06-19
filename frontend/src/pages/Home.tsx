cfc239c0c72cca0f9aa9361b7fc563726d238e0c
PC <melmajouti@gmail.com>
2024-06-19 22:37:21 +0200

    fixed broken homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,38 +1,51 @@
+import RecentChanges from '../components/RecentChanges/RecentChanges';
 import { FC, ReactElement } from 'react';
-import { Section, getAllSections } from '../api/section'; // Adjust the path as necessary
+import { getSelf } from '@/src/api/self';
+import { useGlobalContext } from '@/src/stores/GlobalStore/useGlobalStore';
 import { useQuery } from '@tanstack/react-query';
 
 const Home: FC = (): ReactElement => {
-    // const { data, isError, isLoading } = useQuery({
-    //     queryKey: ['sections', 9012],
-    //     queryFn: getSection,
-    // });
+    const { userCode } = useGlobalContext((state) => ({
+        userCode: state.userCode,
+    }));
 
-    const { data, isError, isLoading } = useQuery({
-        queryKey: ['sections'],
-        queryFn: getAllSections,
+    const {
+        data: self,
+        isLoading: selfLoading,
+        isError: selfError,
+    } = useQuery({
+        queryKey: ['self', userCode],
+        queryFn: getSelf,
     });
 
+    if (selfLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (selfError || !self) {
+        return <div>Error...</div>;
+    }
+
     return (
-        <main>
-            <h1 className='text-4xl font-bold'>All sections</h1>
-            {isLoading && <div>Loading...</div>}
-            {isError && <div>Error</div>}
-            {/* {data && <div data-testid='home-account-data'>{JSON.stringify(data)}</div>} */}
-            {data && (
-                <div className="p-16">
-                    <ul className="flex flex-wrap gap-4 justify-start">
-                        {data.map((section: Section) => (
-                            <li key={section.id} className="bg-neutral-100 w-40 h-40 rounded p-3 hover:bg-blue-50 border border-gray-300 flex-shrink-0">
-                                <a className="hover:underline font-medium" href="#">{section.name}</a>
-                                <p>id: {section.id}</p>
-                                {/* Render other properties as needed */}
-                            </li>
-                        ))}
-                    </ul>
+        <div className='w-full h-[calc(100dvh-56px)] md:h-[calc(100dvh-80px)] flex justify-center items-center'>
+            <div className='flex gap-12 flex-nowrap lg:flex-row flex-col'>
+                <div className='text-justify max-w-[500px]'>
+                    <h1 className='text-4xl tracking-tight mb-4'>Home</h1>
+                    <h2 className='text-2xl mb-2'>Welcome back, {self.name}!</h2>
+                    <p className='text-base'>
+                        You have just opened the Canvas Archive utility. This utility allows you to compare changes made
+                        to a Canvas course.
+                        <br />
+                        <br />
+                        To get started, select a course material from the navigation bar. You can also view recent
+                        changes made to the course in the table.
+                    </p>
                 </div>
-            )}        
-            </main>
+                <div className='w-[500px] shrink lg:shrink-0'>
+                    <RecentChanges />
+                </div>
+            </div>
+        </div>
     );
 };
 export default Home;
