ec11feffd5ce3f7b53596bd8105f096e19315b2e
PC <melmajouti@gmail.com>
2024-06-19 22:22:32 +0200

    fixed hover background color

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -10,7 +10,7 @@ const SectionComponent: FC<SectionComponentProps> = ({ data }) => {
         <div className="p-16">
             <ul className="flex flex-wrap gap-4 justify-start">
                 {data.map((section: Section) => (
-                    <li key={section.id} className="bg-card text-card-foreground w-40 h-40 rounded p-3 hover:bg-blue-50 border border-gray-300 flex-shrink-0">
+                    <li key={section.id} className="bg-card text-card-foreground w-40 h-40 rounded p-3 hover:bg-secondary border border-gray-300 flex-shrink-0">
                         <a className="hover:underline font-medium" href="#">{section.name}</a>
                         <p>id: {section.id}</p>
                     </li>
