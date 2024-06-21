439ac3650fdd5346177682702eb58859c2a0e4af
Mohamed <melmajouti@gmail.com>
2024-06-21 19:43:20 +0200

    fixed code

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -1,23 +1,40 @@
-import React, { FC } from 'react';
-import { Section } from '../api/section';
+import { Change } from '@/src/api/change';
 
-interface SectionComponentProps {
-    data: Section[];
-}
-
-const SectionComponent: FC<SectionComponentProps> = ({ data }) => {
-    return (
-        <div className="p-16">
-            <ul className="flex flex-wrap gap-4 justify-start">
-                {data.map((section: Section) => (
-                    <li key={section.id} className="bg-card text-card-foreground w-40 h-40 rounded p-3 hover:bg-secondary border border-gray-300 flex-shrink-0">
-                        <a className="hover:underline font-medium" href="#">{section.name}</a>
-                        <p>id: {section.id}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+type ChangeWrapper = {
+    change: Change;
 };
 
-export default SectionComponent;
+export type Section = {
+    id: number;
+    course_id: number;
+    name: string;
+    start_at: string | null;
+    end_at: string | null;
+    created_at: string;
+    restrict_enrollments_to_section_dates: boolean | null;
+    nonxlist_course_id: number | null;
+    sis_section_id: string | null;
+    sis_course_id: string;
+    integration_id: string | null;
+};
+
+export default function SectionDiff({ change }: ChangeWrapper) {
+    const sectionJSON = JSON.parse(change.diff);
+    // normalizes incoming json object
+    const sectionData = Array.isArray(sectionJSON) ? sectionJSON: [sectionJSON];
+    
+    return (
+        <div className="overflow-y-auto pb-40">
+            <div className="p-16">
+                <ul className="flex flex-wrap gap-4 justify-start">
+                    {sectionData.map((section: Section) => (
+                        <li key={section.id} className="bg-card text-card-foreground w-40 h-40 rounded p-3 hover:bg-secondary border border-gray-300 flex-shrink-0">
+                            <a className="hover:underline font-medium" href="#">{section.name}</a>
+                            <p>id: {section.id}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>    
+    );
+};
--- frontend/src/mock/change-mock.ts
+++ frontend/src/mock/change-mock.ts
@@ -48,13 +48,146 @@ const generateChange = (old_value: number): Change => {
     const randomWeeks = faker.number.int({ min: 0, max: 7 }); // Random number of weeks within 8 week period
     const randomDate = setDay(addWeeks(startDate, randomWeeks), 1); // Set the day to Monday
 
+    const diffJSON = [
+        {
+            "id": 242604,
+            "course_id": 5285,
+            "name": "aaaaa",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-17T11:31:25Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242605,
+            "course_id": 5285,
+            "name": "aaaaa",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:19:32Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242608,
+            "course_id": 5285,
+            "name": "aaaaa",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:20:20Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242609,
+            "course_id": 5285,
+            "name": "aaaaa",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:20:20Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 9012,
+            "course_id": 5285,
+            "name": "Atoomfysica 1",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2019-03-19T18:57:10Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": "1800.1040",
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242606,
+            "course_id": 5285,
+            "name": "Atoomfysica 1",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:19:32Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242610,
+            "course_id": 5285,
+            "name": "Atoomfysica 1",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:20:20Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242611,
+            "course_id": 5285,
+            "name": "Atoomfysica 1",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:20:20Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242603,
+            "course_id": 5285,
+            "name": "Atoomfysica 1 2024-06-17",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-17T11:29:01Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        },
+        {
+            "id": 242607,
+            "course_id": 5285,
+            "name": "Atoomfysica 1 2024-06-17",
+            "start_at": null,
+            "end_at": null,
+            "created_at": "2024-06-18T12:19:32Z",
+            "restrict_enrollments_to_section_dates": null,
+            "nonxlist_course_id": null,
+            "sis_section_id": null,
+            "sis_course_id": "1800.1040",
+            "integration_id": null
+        }
+    ];
+
     return {
         id: faker.number.int(),
         old_id: old_value,
         change_type: faker.helpers.arrayElement(Object.values(ChangeType)),
         item_type: faker.helpers.arrayElement(Object.values(ItemTypes)),
         timestamp: randomDate.toString(),
-        diff: `<div class='bg-card text-card-foreground w-full h-full flex justify-center items-center'><p class='text-2xl font-bold'>This is a placeholder for the change view.</p></div>`,
+        diff: JSON.stringify(diffJSON),
     };
 };
 
